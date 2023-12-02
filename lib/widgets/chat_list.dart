72a1cafd854ff85a96dd6d5ad8b68b01ee480dbd
Your Name <your_email@example.com>
2023-12-02 16:53:40 +0200

    firebase cloud 4.13.3, success

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/widgets/components/chat_drawer.dart';
 import 'package:chat_app/widgets/components/chat_list_item.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatListScreen extends StatelessWidget {
@@ -32,16 +33,43 @@ class ChatListScreen extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.surface,
       ),
       endDrawer: const ChatDrawer(),
-      body: ListView.builder(
-        itemCount: 10, // This will be dynamic later
-        itemBuilder: (context, index) {
-          return ChatListItem(
-            chatPartnerName: 'Chat Partner $index',
-            lastMessage: 'Last message snippet...',
-            imageUrl:
-                'https://via.placeholder.com/150', // Placeholder image URL
-            timestamp: '9:40 AM',
-            unreadMessages: index % 2, // Example for unread messages
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('chats').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("Waiting for data...");
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            print("Error fetching data: ${snapshot.error}");
+            return Center(child: Text('Something went wrong'));
+          }
+
+          if (!snapshot.hasData) {
+            print("No data available");
+            return Center(child: Text('No chats found'));
+          }
+
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var chatData =
+                  snapshot.data!.docs[index].data() as Map<String, dynamic>;
+              // Use fields from your Firestore documents. Adjust the field names as per your database structure
+              return ChatListItem(
+                chatPartnerName: chatData['partnerName'] ??
+                    'Άγνωστος', // Adjust field names based on your Firestore
+                lastMessage: chatData['lastMessage'] ?? 'Δεν υπάρχει μήνυμα',
+                imageUrl:
+                    chatData['imageUrl'] ?? 'https://via.placeholder.com/150',
+                timestamp: (chatData['lastMessageTimestamp'] as Timestamp)
+                    .toDate()
+                    .toString(), // Format timestamp as needed
+                unreadMessages:
+                    0, // Implement logic to count unread messages if needed
+              );
+            },
           );
         },
       ),
--- lib/widgets/components/chat_drawer.dart
+++ lib/widgets/components/chat_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatDrawer extends StatelessWidget {
@@ -5,6 +6,12 @@ class ChatDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+    final userEmail =
+        user?.email ?? 'No Email'; // Default text if the email is null
+// You can also retrieve the user's photo URL if you have set it up
+    final userPhotoUrl = user?.photoURL ?? 'default_photo_url';
+
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.background,
       width: 250,
@@ -16,33 +23,57 @@ class ChatDrawer extends StatelessWidget {
               color: Theme.of(context).colorScheme.surface,
               // Add more decoration as needed
             ),
-            child: Text('Drawer Header',
-                style: Theme.of(context).textTheme.bodyLarge),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      userPhotoUrl), // Replace with actual photo URL if available
+                  radius: 40.0, // Adjust the size as needed
+                ),
+                const SizedBox(height: 10), // Space between avatar and text
+                Text(
+                  userEmail, // Display the user's email or in-app tag
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ],
+            ),
           ),
           ListTile(
-            leading: Icon(Icons.message),
-            title: Text('Chats', style: Theme.of(context).textTheme.bodyLarge),
+            leading: Icon(
+              Icons.account_circle,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            title: Text(
+              'Προφίλ',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
             onTap: () {
               // Handle the tap
             },
           ),
           ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Profile'),
+            leading: Icon(
+              Icons.edit_document,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            title: Text(
+              'Οι σημειώσεις μου',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
             onTap: () {
               // Handle the tap
             },
           ),
           ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Οι σημειώσεις μου'),
-            onTap: () {
-              // Handle the tap
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.account_circle),
-            title: Text('Ρυθμίσεις'),
+            leading: Icon(
+              Icons.settings_rounded,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            title: Text(
+              'Ρυθμίσεις',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
             onTap: () {
               // Handle the tap
             },
